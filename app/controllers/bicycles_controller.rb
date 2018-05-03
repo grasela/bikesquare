00c3d84d29a781b2e88eee6fb48687d0fe9b004d
Eryk Grasela <grasela.eryk@gmail.com>
2018-05-03 10:59:31 +1000

    Add main route and amended before action for the bicycle controller

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -1,4 +1,5 @@
 class BicyclesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_bicycle, only: [:show, :edit, :update, :destroy]
 
   # GET /bicycles
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :bicycles
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'bicycles#index'
+
 end
