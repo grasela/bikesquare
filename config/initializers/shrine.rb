a1c8d76597bb468a388d3edf83eb07e9fe74d1cb
Eryk Grasela <grasela.eryk@gmail.com>
2018-05-11 03:02:30 +1000

    Fix shrine loading cache

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,13 +1,10 @@
 require "shrine"
-# require "shrine/storage/file_system"
+require "shrine/storage/file_system"
 
 # Shrine.storages = {
 #   cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
 #   store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"), # permanent
 # }
-
-# Shrine.plugin :activerecord
-# Shrine.plugin :cached_attachment_data # for forms
 
 
 require "shrine/storage/s3"
@@ -23,3 +20,6 @@ Shrine.storages = {
   cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
   store: Shrine::Storage::S3.new(**s3_options),
 }
+Shrine.plugin :activerecord
+Shrine.plugin :cached_attachment_data # for forms
+
